4ec9ca357fa7be1ef782c6734ca01da4d12604d0
ldclakmal <ldclakmal@gmail.com>
2018-05-20 16:20:42 +0530

    Setup initial structure of webservice

---
+++ webservice/src/main/java/org/rotaract/alumni/mora/mapmytrain/controller/Main.java
@@ -0,0 +1,28 @@
+package org.rotaract.alumni.mora.mapmytrain.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Chanaka Lakmal
+ * @since 1.0
+ */
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Main extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Main.class);
+    }
+}
