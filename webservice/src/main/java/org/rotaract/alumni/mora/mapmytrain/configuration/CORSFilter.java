d050977266f4753602efe773534cb63e9ae19b84
ldclakmal <ldclakmal@gmail.com>
2018-05-20 16:21:38 +0530

    Add cors filters

---
+++ webservice/src/main/java/org/rotaract/alumni/mora/mapmytrain/configuration/CORSFilter.java
@@ -0,0 +1,36 @@
+package org.rotaract.alumni.mora.mapmytrain.configuration;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Chanaka Lakmal
+ * @since 1.0
+ */
+
+@Component
+public class CORSFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
